404aea425e92664a71149617564aee5f7a6aaa73
Tony Gadaleta <anthonygadaleta@gmail.com>
2025-04-30 17:25:12 -0400

    Add CORS support and update URL patterns for user profile access

--- BariatricCookbookServer/BariatricCookbookServer/settings.py
+++ BariatricCookbookServer/BariatricCookbookServer/settings.py
@@ -45,9 +45,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'BariatricCookbook',
     'rest_framework.authtoken',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -56,6 +58,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # Vite default dev server
+]
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'BariatricCookbookServer.urls'
 
