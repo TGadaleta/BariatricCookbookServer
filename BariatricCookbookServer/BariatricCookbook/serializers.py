2fb9120afa4c4618ba321cdd967e3e10f3456231
Tony Gadaleta <anthonygadaleta@gmail.com>
2025-05-01 11:57:21 -0400

    Refactor UserSerializer validation to ensure unique username and email

--- BariatricCookbookServer/BariatricCookbook/serializers.py
+++ BariatricCookbookServer/BariatricCookbook/serializers.py
@@ -14,8 +14,14 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'password']
-        extra_kwargs = {'email': {'validations': []}}
-    
+
+    def validate(self, data):
+        if User.objects.filter(username=data['username']).exists():
+            raise serializers.ValidationError({'username': 'This username is already taken.'})
+        if User.objects.filter(email=data['email']).exists():
+            raise serializers.ValidationError({'email': 'This email is already in use.'})
+        return data
+
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
